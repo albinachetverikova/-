e88aad5a380f4c9f1be3ea20e0a3ed37941e18c0
albinachetverikova <albinacetverikova3@gmail.com>
2024-05-16 21:50:17 +0300

    Update ColoredPoint.cs

--- ColoredPoint.cs
+++ ColoredPoint.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace ConsoleApp5
 {
     // Класс ColoredPoint наследуется от Point и добавляет свойство цвета
-    internal class ColoredPoint:Point
+    internal class ColoredPoint : Point
     {
-        // Свойство для цвета точки
         public string Color { get; set; }
-
-        // Конструктор с параметрами позволяет задать координаты и цвет точки
+        
         public ColoredPoint(double x = 0, double y = 0, string color = "black") : base(x, y)
         {
             Color = color;
