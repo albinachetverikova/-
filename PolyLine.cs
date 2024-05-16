7d489229e278903a2da263493093bf644defb5ea
albinachetverikova <albinacetverikova3@gmail.com>
2024-05-16 21:59:03 +0300

    Update PolyLine.cs

--- PolyLine.cs
+++ PolyLine.cs
@@ -1,27 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp5
 {
     // Класс PolyLine представляет многоугольник, состоящий из серии линий
-    internal class PolyLine:Line
+    internal class PolyLine
     {
-        // Список точек, формирующих многоугольник
         public List<Point> Points { get; set; }
-
-        // Конструктор с параметрами позволяет задать список точек многоугольника
+        
         public PolyLine(List<Point> points = null)
         {
             Points = points ?? new List<Point> { new Point() };
         }
-
+        
         // Метод Scale масштабирует многоугольник на заданный коэффициент
         public void Scale(double factor)
         {
-            // Реализация метода масштабирования
+            for (int i = 0; i < Points.Count; i++)
+            {
+                Points[i].X *= factor;
+                Points[i].Y *= factor;
+            }
         }
     }
 }
